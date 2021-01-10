1a8290c378d02822b3435aa5457ff485d76d92e3
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-09 21:33:54 -0800

    drops card using useDrop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React, { useState } from 'react'
 import './App.css';
 import { Box } from './components/Box';
 
 function App() {
+  // track index of box for DnD
+  const [index, setIndex] = useState(1);
+
+  // find the index of the card and set it to state
+  const moveCard = ((index) => {
+    setIndex(index)
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
         {/* Pass a card - should display "Card" */}
-        <Box card />
+        <Box card={index === 1} moveCard={moveCard.bind(null, 1)} />
         {/* no props - should display "empty card" */}
-        <Box />
-        <Box />
+        < Box card={index === 2} moveCard={moveCard.bind(null, 2)} />
+        <Box card={index === 3} moveCard={moveCard.bind(null, 3)} />
 
         {/* <ol>
           <List wordsList={draggableList} />
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Card } from './Card'
+import { useDrop } from 'react-dnd'
 
-export const Box = ({ card }) => {
+export const Box = ({ card, moveCard }) => {
+  // useDrop takes in an obj with 3 props
+  const [{ isOver }, dropRef] = useDrop({
+    // cards can ("accept") be dropped in this box - "card" also in useDrag hook
+    accept: "card",
+    // this is trigged when card is dropped - moveCard() is passed to box component
+    drop: () => moveCard(),
+    // checks if card isOver a box, if so, set isOver to true
+    collect: (monitor) => ({
+      isOver: monitor.isOver()
+    })
+  });
   return (
     <>
-      <div className="box">{card ? <Card /> : "empty card"}</div>
+      <div
+        className="box"
+        ref={dropRef}
+        style={{ backgroundColor: isOver ? "white" : "black" }}>{card ? <Card /> : "empty card"}</div>
     </>
   )
 }
